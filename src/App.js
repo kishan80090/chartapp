961cc33cee5718e57ab4a3122f56b21de6136ed1
kishan955971 <kishan221314z@gmail.com>
2025-05-10 11:34:13 +0530

    chart

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
- import logo from "./logo.svg";
-import "./App.css";
-import LineChart from "./components/LineChart";
+ import "./App.css";
+import LineChart from "./component/LineChart";
 function App() {
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    data: [1, 2, 3, 4, 5, 6],
+    labels: ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"],
+    data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     title:"My Line"
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <LineChart data={data} />
     </div>
   );
 }
+
+export default App;
